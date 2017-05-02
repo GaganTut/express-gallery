1ccb41fea677fe87b8d3debed51587de08e198c9
GaganTut <gstut@live.com>
2017-05-01 15:34:23 -1000

    loaded gulp/sass

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  gulp.watch("scss/**/*.scss", ['sass']);
+  return gulp.src("scss/styles.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("public/css"));
+});
+
+gulp.task('default', ['sass']);
